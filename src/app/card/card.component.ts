44a894e5fec4cca2827d94fdb13d2b6741a9bb19
Paraschiv George <georgegmtt@gmail.com>
2018-10-20 20:06:18 +0300

    added drag-and-drop library added board component modified card component to get card interface as input simulated a drag-and-drop scenario in the board component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Card } from '../interfaces/card';
 
 @Component({
@@ -10,22 +10,13 @@ export class CardComponent implements OnInit {
 
   constructor() { }
 
-  card: Card;
+  @Input() card: Card;
   width: number;
   height: number;
 
   ngOnInit() {
-    this.card = {
-      name: 'Macanache',
-      heatlh: 8,
-      attack: 8,
-      cost: 8,
-      description: 'Cum te bate MACANACHE nu te bate Dumnezeu.',
-      team: 'Boschetar',
-      photo_url: 'https://hearthcards.ams3.digitaloceanspaces.com/33/58/8d/5b/33588d5b.png'
-    };
-    this.width = 400;
-    this.height = 600;
+    this.width = 200;
+    this.height = 300;
   }
 
 }
